cdde8406c61a63fd3784293d70289fb548926dcc
Sun Graham <sungraham7@gmail.com>
2020-06-26 11:23:11 -0400

    FINAL HOMESCREEN

--- FinalProject/ViewController.swift
+++ FinalProject/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var allyChecklist: UIButton!
+    
+    @IBOutlet weak var dictionary: UIButton!
+    
+    @IBOutlet weak var brands: UIButton!
+    
+    @IBOutlet weak var names: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        allyChecklist.titleLabel!.font = UIFont.init(name: "WalkwayBlack", size: 24)
+    
+        dictionary.titleLabel!.font = UIFont.init(name: "WalkwayBlack", size: 24)
+        
+       brands.titleLabel!.font = UIFont.init(name: "WalkwayBlack", size: 24)
+        
+        names.font = UIFont.init(name: "WalkwayBlack", size: 14)
     }
 
 
